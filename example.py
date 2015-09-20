05fbbaaf83460e00df22cbd4d9db50b4fab6a6c6
Michael V. DePalatis <mike@depalatis.net>
2015-09-20 14:12:50 +0200

    Add basic example

---
+++ example.py
@@ -0,0 +1,23 @@
+"""A simple example of using the ``EventSource`` handler. To see
+streaming data, do::
+
+  $ curl http://localhost:9000
+
+"""
+
+import random
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.web import Application
+from tornadose.handler import EventSource
+from tornadose.stores import DataStore
+
+store = DataStore()
+
+app = Application(
+    [(r'/', EventSource, {'source': store})],
+    debug=True)
+app.listen(9000)
+
+loop = IOLoop.instance()
+PeriodicCallback(lambda: store.set_data(random.random()), 1000, loop).start()
+loop.start()
