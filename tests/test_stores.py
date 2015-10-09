ada67593599ef329a4550f8c4f03ab22d6e0c631
Michael V. DePalatis <mike@depalatis.net>
2015-10-09 23:59:35 +0200

    Use tox for testing automation

--- tests/test_stores.py
+++ tests/test_stores.py
@@ -1,5 +1,25 @@
-"""Tests for data stores."""
+"""Tests for stores."""
 
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 import pytest
 
-from tornadose.stores import DataStore, QueueStore
+from tornado.web import Application
+from tornadose.stores import DataStore
+
+
+@pytest.fixture
+def store():
+    return DataStore()
+
+
+def test_submit(store):
+    store.submit('test')
+    assert store.data == 'test'
+
+
+@pytest.mark.gen_test
+def test_publish(store):
+    pass
