3b261d6153a458936aab60f8a82970b2c1ba81f2
Michael V. DePalatis <mike@depalatis.net>
2015-12-08 17:03:41 +0100

    Remove profiling in demo

--- demos/fibonacci.py
+++ demos/fibonacci.py
@@ -9,12 +9,10 @@ from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from tornado.locks import Event
 from tornado.web import Application, RequestHandler
-from tornado.httpclient import AsyncHTTPClient
 from tornadose.handlers import EventSource
 from tornadose.stores import DataStore
 
 define('port', default=9000)
-define('profile', default=False)
 
 html = """
 <div id="messages"></div>
@@ -28,7 +26,6 @@ html = """
 
 store = DataStore()
 finish = Event()
-client = AsyncHTTPClient()
 
 
 def fibonacci():
@@ -53,18 +50,6 @@ def generate_sequence():
 
 
 @gen.coroutine
-def subscribe():
-    """Subscribe to the stream with an AsyncHTTPClient."""
-    def callback(data):
-        print(data)
-
-    if options.profile:
-        yield client.fetch(
-            'http://127.0.0.1:{}/fibonacci'.format(options.port),
-            streaming_callback=callback)
-
-
-@gen.coroutine
 def main():
     app = Application(
         [
@@ -74,18 +59,14 @@ def main():
         debug=True
     )
     app.listen(options.port)
-    yield [generate_sequence(), subscribe()]
+    yield generate_sequence()
 
 
 def shutdown(sig, frame):
     finish.set()
-    client.close()
     IOLoop.instance().stop()
 
 if __name__ == "__main__":
     options.parse_command_line()
     signal.signal(signal.SIGINT, shutdown)
-
-    if options.profile:
-        IOLoop.instance().call_later(10, lambda: shutdown(None, None))
     IOLoop.instance().run_sync(main)
