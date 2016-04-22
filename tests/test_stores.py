5e87c59f1cab7f340b0692f400994657533735cc
Michael V. DePalatis <mike@depalatis.net>
2016-04-22 17:06:19 -0500

    Update store testing

--- tests/test_stores.py
+++ tests/test_stores.py
@@ -3,16 +3,56 @@
 import pytest
 
 from tornado.web import Application
-from tornadose.stores import DataStore, QueueStore
+from tornadose.stores import BaseStore, DataStore, QueueStore, RedisStore
 from utilities import TestHandler
 
-data_store = DataStore()
-queue_store = QueueStore()
+
+class BaseTest:
+    @pytest.fixture
+    def store(self):
+        return None
+
+    @pytest.fixture
+    def app(self):
+        return Application([
+            (r'/store', TestHandler, self.store)
+        ])
+
+    def test_submit(self, store):
+        store.submit('data')
 
 
-@pytest.fixture
-def app():
-    return Application([
-        (r'/datastore', TestHandler, data_store),
-        (r'/queuestore', TestHandler, queue_store)
-    ])
+class TestBaseStore(BaseTest):
+    @pytest.fixture
+    def store(self):
+        return BaseStore()
+
+    def test_submit(self, store):
+        with pytest.raises(NotImplementedError):
+            super(TestBaseStore, self).test_submit(store)
+
+
+class TestDataStore(BaseTest):
+    @pytest.fixture
+    def store(self):
+        return DataStore()
+
+    def test_data_property(self, store):
+        assert store.data is None
+        store.data = 'data'
+        assert store.data == 'data'
+
+
+class TestQueueStore(BaseTest):
+    @pytest.fixture
+    def store(self):
+        return QueueStore()
+
+
+class TestRedisStore(BaseTest):
+    @pytest.fixture
+    def store(self):
+        return RedisStore(channel='tornadose-test')
+
+    def test_submit(self, store):
+        store.submit('data', debug=True)
