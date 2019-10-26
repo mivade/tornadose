75a5dc2b4a23beaef3095ef709d8b2b21367638f
Michael V. DePalatis <mike@depalatis.net>
2019-10-26 12:01:41 -0600

    Fix websocket test

--- tests/test_websocket_subscriber.py
+++ tests/test_websocket_subscriber.py
@@ -14,19 +14,18 @@ from tornadose.handlers import WebSocketSubscriber
 @pytest.fixture
 def app(dummy_store) -> Application:
     app = Application([
-        (r'/', WebSocketSubscriber, dict(store=dummy_store))
+        (r"/", WebSocketSubscriber, dict(store=dummy_store))
     ])
     return app
 
 
 class TestWebSocketSubscriber:
-    @pytest.mark.gen_test(timeout=1)
-    async def test_get_message(self, http_server, base_url, dummy_store):
+    @pytest.mark.gen_test
+    async def test_get_message(self, http_server, io_loop, base_url, dummy_store):
         url = base_url.replace("http://", "ws://")
-        breakpoint()
-        conn = await websocket_connect(url)
+        conn = await websocket_connect(url, connect_timeout=1)
         dummy_store.submit('test')
-        asyncio.get_event_loop().call_soon(dummy_store.publish)
+        io_loop.add_callback(dummy_store.publish)
         msg = await conn.read_message()
         msg = json.loads(msg)
         assert msg["data"] == "test"
